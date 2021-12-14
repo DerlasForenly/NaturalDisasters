8841d1a2a9032621136aca1322165d1daf13b382
Serhii Vlokh <tany.tany283@gmail.com>
2021-12-14 14:19:45 +0200

    ref

--- natural_disasters_front/src/store/disastersModule.js
+++ natural_disasters_front/src/store/disastersModule.js
@@ -59,29 +59,8 @@ const disastersModule = {
             commit('prevPage')
             dispatch('fetchDisasters')
 		},
-        async postDisasters({state, commit}) {
-            try {
-				commit('setDisastersSaving', true)
-				const response = await axios({
-					method: 'post',
-					url: 'http://localhost:8000/api/events',
-					data: {
-						events: state.disasters
-					}
-				})
-      
-              	commit('addMessage', response.data.message)
-				commit('setDisastersSaving', false)
-				console.log('Events have been saved to database!')
-            } catch (e) {
-                commit('addMessage', e.message)
-            } finally {
-				commit('setDisastersSaving', false)
-            }
-          },
         async fetchDisasters({state, commit, dispatch}) {
             try {
-                console.log(state.category)
                 commit('setLoading', true)
                 const response = await axios.get(`http://localhost:8000/api/events?page=${state.page}&limit=${state.lines}&category=${state.category}`)
                 commit('setDisasters', response.data.events.data)
@@ -90,9 +69,6 @@ const disastersModule = {
             } finally {
                 commit('setLoading', false)
             }
-        },
-        async fetchFilteredDisasters({state, commit}) {
-
         },
     },
     namespaced: true,
